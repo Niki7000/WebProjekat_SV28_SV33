2774fb0cbf40d8f64a2eaf104b9dacfca7f70ae3
Niki7000 <petefi.nikodingregorin@gmail.com>
2026-06-08 20:59:16 +0200

    Додате рецензије из базе за сваку књигу.

--- js/knjiga.js
+++ js/knjiga.js
@@ -2,6 +2,8 @@ let firebaseUrl = 'https://webprojekat-sv28-sv33-default-rtdb.europe-west1.fireb
 
 let books = {};
 let authors = {};
+let reviews = {};
+let users = {};
 
 let urlParams = new URLSearchParams(window.location.search);
 
@@ -9,18 +11,21 @@ let bookId = urlParams.get('id');
 
 Promise.all([
     fetch(firebaseUrl + '/knjige.json'),
-    fetch(firebaseUrl + '/autori.json')
+    fetch(firebaseUrl + '/autori.json'),
+    fetch(firebaseUrl + '/recenzije.json'),
+    fetch(firebaseUrl + '/korisnici.json')
 ])
 .then(responses => Promise.all(
     responses.map(r => r.json())
 ))
 .then(data => {
-
     books = data[0];
     authors = data[1];
+    reviews = data[2];
+    users = data[3];
 
     prikaziKnjigu();
-
+    prikaziRecenzije();
 });
 
 function prikaziKnjigu() {
@@ -70,3 +75,72 @@ function prikaziKnjigu() {
     authorLink.href =
         "autor.html?id=" + knjiga.idAutora;
 }
+
+function prikaziRecenzije() {
+    let container =  document.getElementById('reviews-container');
+    container.innerHTML = '';
+    let brojRecenzija = 0;
+
+    for(let id in reviews){
+        let review = reviews[id];
+
+        if(review.idKnjige != bookId){
+            continue;
+        }
+
+        brojRecenzija++;
+
+        let korisnik = users[review.idKorisnika];
+
+        let imeKorisnika = korisnik ? korisnik.ime + ' ' + korisnik.prezime : 'Непознат корисник';
+
+        container.innerHTML += `
+            <div class="card p-3 mb-3">
+
+                <h6>
+                    ${imeKorisnika}
+                </h6>
+
+                <small class="text-muted">
+                    ${review.datum}
+                </small>
+
+                <p class="mt-2 mb-0">
+                    ${review.tekst}
+                </p>
+
+            </div>
+        `;
+    }
+
+    if(brojRecenzija == 0){
+
+        container.innerHTML = `<p>Још увек нема рецензија за ову књигу.</p>`;
+    }
+}
+
+document.getElementById('review-form').addEventListener('submit',
+    function(e){
+        e.preventDefault();
+
+        let text =document.getElementById('review-text').value.trim();
+        let error = document.getElementById('review-error');
+        error.innerHTML = '';
+
+        if(text.length < 10){
+            error.innerHTML = 'Рецензија мора имати најмање 10 карактера.';
+            return;
+        }
+
+        reviews['temp' + Date.now()] = {
+            datum: new Date().toISOString().split('T')[0],
+            idKnjige: bookId,
+            idKorisnika: 'kor001',
+            tekst: text
+        };
+
+        prikaziRecenzije();
+
+        this.reset();
+    }
+);
