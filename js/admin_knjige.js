9b7c4d79e61aeb441972dba3643a37805e31f3f9
Niki7000 <petefi.nikodingregorin@gmail.com>
2026-06-20 11:28:22 +0200

    Брисање и ажурирање књига из базе + побољшано додавање.

--- js/admin_knjige.js
+++ js/admin_knjige.js
@@ -3,6 +3,7 @@ let firebaseUrl = 'https://webprojekat-sv28-sv33-default-rtdb.europe-west1.fireb
 let books = {};
 let bookIds = [];
 let authors = {};
+let selectedBookId = null;
 
 let booksLoaded = false;
 let authorsLoaded = false;
@@ -16,6 +17,23 @@ function prikaziKnjige() {
     for(let id of bookIds){
 
         let knjiga = books[id];
+        let authorOptions = "";
+
+        for(let authorId in authors){
+
+            let autor = authors[authorId];
+
+            let selected =
+                authorId === knjiga.idAutora
+                ? "selected"
+                : "";
+
+            authorOptions += `
+                <option value="${authorId}" ${selected}>
+                    ${autor.ime} ${autor.prezime}
+                </option>
+            `;
+        }
 
         container.innerHTML += `
         
@@ -33,7 +51,7 @@ function prikaziKnjige() {
 
                     <div class="col-12 col-md-9">
 
-                        <form class="book-form">
+                        <form class="book-form" data-id="${id}">
 
                             <div class="row g-3">
 
@@ -85,9 +103,12 @@ function prikaziKnjige() {
                                 </div>
 
                                 <div class="col-md-4">
-                                    <label class="form-label">Аутор</label>
-
-                                    <input type="text" class="form-control" value="${authors[knjiga.idAutora].ime} ${authors[knjiga.idAutora].prezime}">
+                                    <label class="form-label">
+                                        Аутор
+                                    </label>
+                                    <select class="form-select author-select">
+                                        ${authorOptions}
+                                    </select>
                                 </div>
 
                                 <div class="col-12">
@@ -101,7 +122,7 @@ function prikaziKnjige() {
                             </div>
 
                             <button type="button"
-                                    class="btn btn-custom mt-3">
+                                    class="btn btn-custom mt-3 update-btn">
                                 Ажурирај
                             </button>
 
@@ -110,7 +131,9 @@ function prikaziKnjige() {
                                 Обриши
                              </button>
 
-                        </form>
+                             </form>
+
+                            <h3 id="poruka-azuriranja-${id}"></h3>
 
                     </div>
 
@@ -166,6 +189,8 @@ requestAuthors.onload = function(){
 
     authorsLoaded = true;
 
+    popuniAutorSelectZaDodavanje();
+
     if(booksLoaded){
         prikaziKnjige();
     }
@@ -207,6 +232,8 @@ function postaviDeleteDugmad(){
     deleteButtons.forEach(button => {
 
         button.addEventListener('click', function(){
+
+            selectedBookId = this.dataset.id;
 
             let modal =
                 new bootstrap.Modal(
@@ -318,7 +345,8 @@ function validirajDodavanje() {
                 format: format.value,
                 cena: Number(cena.value),
                 brojStrana: Number(brojStrana.value),
-                isbn: isbn.value
+                isbn: isbn.value,
+                idAutora: idAutora.value
             };
             dodajNovuKnjigu(novaKnjiga);
         }
@@ -330,44 +358,66 @@ function validirajDodavanje() {
 
 }
 
-validirajDodavanje();
 
 function postaviUpdateValidaciju(){
-
-    let updateButtons =
-        document.querySelectorAll('.btn-custom');
+    let updateButtons = document.querySelectorAll('.update-btn');
 
     updateButtons.forEach(button => {
-
         button.addEventListener('click', function(){
+            let form = this.closest('.book-form');
+            let validno = true;
+            let polja = form.querySelectorAll('input, textarea, select');
 
-            let form =
-                this.closest('.book-form');
+            polja.forEach(polje => {
+                if(polje.value.trim() === ''){
+                    polje.classList.remove('is-valid');
+                    polje.classList.add('is-invalid');
+                    validno = false;
+                }
+                else{
+                    polje.classList.remove('is-invalid');
+                    polje.classList.add('is-valid');
+                }
+            });
 
-            let isbn =
-                form.querySelector('.isbn-input');
+            let isbn = form.querySelector('.isbn-input');
 
-            let regex =
-                /^(97[89])([-]?[0-9]){10}$/;
+            let regex = /^(97[89])([-]?[0-9]){10}$/;
 
-            if(regex.test(isbn.value)){
-
-                isbn.classList.remove('is-invalid');
-                isbn.classList.add('is-valid');
-
-                let poruka = document.getElementById('poruka').innerHTML = "Подаци су исправни.";
-            }
-            else{
-
+            if(!regex.test(isbn.value)){
                 isbn.classList.remove('is-valid');
                 isbn.classList.add('is-invalid');
 
-                let poruka = document.getElementById('poruka').innerHTML = "Подаци нису су исправни.";
-
+                validno = false;
             }
 
-        });
+            if(validno){
+                let bookId = form.dataset.id;
 
+                let inputs = form.querySelectorAll('.form-control');
+
+                let authorId = form.querySelector('.author-select').value;
+
+                let izmenjenaKnjiga = {
+                    naziv: inputs[0].value,
+                    zanr: inputs[1].value,
+                    opis: inputs[2].value,
+                    slike: [inputs[3].value],
+                    format: inputs[4].value,
+                    cena: Number(inputs[5].value),
+                    brojStrana: Number(inputs[6].value),
+                    isbn: inputs[7].value,
+                    idAutora: authorId
+                };
+
+                azurirajKnjigu(bookId,izmenjenaKnjiga);
+            }
+            else{
+                let bookId = form.dataset.id;
+                let poruka = document.getElementById('poruka-azuriranja-'+bookId);
+                poruka.innerHTML = 'Нисте исправно унели податке за ажурирање. ';
+            }
+        });
     });
 
 }
@@ -402,7 +452,7 @@ function dodajNovuKnjigu(novaKnjiga){
 function generisiNoviBookId(){
     let maxBroj = 0;
 
-    for(let id in bookIds){
+    for(let id of bookIds){
         let broj = parseInt(id.replace('knj',''));
         
         if(broj > maxBroj) maxBroj = broj;
@@ -411,3 +461,65 @@ function generisiNoviBookId(){
     maxBroj += 1;
     return `knj` + String(maxBroj).padStart(3, '0');
 }
+
+function postaviPotvrduBrisanja(){
+    let confirmBtn = document.getElementById('confirm-delete-btn');
+
+    confirmBtn.addEventListener('click', function(){
+        if(!selectedBookId) return;
+
+        fetch(firebaseUrl + '/knjige/' + selectedBookId + '.json', 
+            {
+                method: 'DELETE'
+            }
+        )
+        .then(() => location.reload())
+        .catch(error => {
+            console.log(error);
+        })
+    });
+}
+
+function azurirajKnjigu(bookId, knjiga){
+    let poruka = document.getElementById('poruka-azuriranja-'+bookId);
+    
+    fetch(
+        firebaseUrl + '/knjige/' + bookId + '.json',
+        {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(knjiga)
+        }
+    )
+    .then(response => response.json())
+    .then(data => {
+        poruka.innerHTML = 'Књига је успешно ажурирана!';
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+function popuniAutorSelectZaDodavanje(){
+    if(!authors) return;
+
+    let autoriSelect = document.getElementById('idAutora');
+    let authorOptions = '<option value="">Изаберите аутора</option>';
+
+    for(let authorId in authors){
+        let autor = authors[authorId];
+
+        authorOptions += `
+            <option value="${authorId}">
+                ${autor.ime} ${autor.prezime}
+            </option>
+        `;
+    }
+    autoriSelect.innerHTML = authorOptions;
+}
+
+
+validirajDodavanje();
+postaviPotvrduBrisanja();
