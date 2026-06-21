bb6f6e2e07b8e2b0a35f382a1a5cf305fdab5da8
OgnjanOstovic <ostovic.sv33.2025@uns.ac.rs>
2026-06-21 14:53:28 +0200

    Dodat search sistem za autore

--- js/autori.js
+++ js/autori.js
@@ -1,6 +1,9 @@
 var firebaseUrl = 'https://webprojekat-sv28-sv33-default-rtdb.europe-west1.firebasedatabase.app';
 var authorIds = [];
 var authors = {};
+var searchInput = null;
+var searchBtn = null;
+var currentSearchQuery = '';
 
 var request = new XMLHttpRequest();
 
@@ -8,36 +11,109 @@ request.open('GET', firebaseUrl + '/autori.json');
 request.send();
 
 request.onload = function () {
-    if (this.readyState == 4) {
-        if (this.status == 200) {
-            authors = JSON.parse(request.responseText);
-            for (var id in authors) {
-                authorIds.push(id);
-            }
-            dodajAutora();
+    if (this.readyState == 4 && this.status == 200) {
+        authors = JSON.parse(request.responseText);
+
+        for (var id in authors) {
+            authorIds.push(id);
         }
+
+        prikaziAutore(authorIds);
+        setupSearch();
+    }
+};
+
+function prikaziAutore(listaAutorId) {
+    var red = document.getElementById('autor-list');
+    red.innerHTML = '';
+
+    if (!listaAutorId || listaAutorId.length === 0) {
+        red.innerHTML = '<div class="col-12"><p class="text-center mt-4">Нема резултата за претрагу.</p></div>';
+        return;
+    }
+
+    for (var i = 0; i < listaAutorId.length; i++) {
+        var id = listaAutorId[i];
+        var autor = authors[id];
+
+        if (!autor) {
+            continue;
+        }
+
+        red.innerHTML += `
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="autor">
+                    <img src="${autor['slike'][0]}" alt="${autor['ime']} ${autor['prezime']}" class="autor-img">
+                    <div class="autor-bio">
+                        <h3>${oznaciTekst(autor['ime'], currentSearchQuery)} ${oznaciTekst(autor['prezime'], currentSearchQuery)}</h3>
+                        <p>${autor['biografija']}</p>
+                        <a href="../pages/autor.html?id=${id}" class="btn btn-light btn-sm btn-1">Више о аутору</a>
+                    </div>
+                </div>
+            </div>`;
     }
 }
 
-function dodajAutora() {
-    var red = document.getElementById('autor-list');
-    var container = document.getElementsByClassName('autor-container')[0];
-    for (var i=0; i < authorIds.length; i+=3) {
-        for (let j=0;j<3;j++)
-        {
-            red.innerHTML += 
-            ` <div class="col-12 col-md-6 col-lg-4">
-                <div class="autor">
-                        <img src="${authors[authorIds[j+i]]['slike'][0]}" alt="Автор 1" class="autor-img">
-                        <div class="autor-bio">
-                            <h3>${authors[authorIds[j+i]]['ime']} ${authors[authorIds[j+i]]['prezime']}</h3>
-                            <p>${authors[authorIds[j+i]]['biografija']}</p>
-                            <a href="../pages/autor.html?id=${authorIds[j+i]}" class="btn btn-light btn-sm btn-1">Више о аутору</a>
-                        </div>
-                </div>
-                </div>`;
-        }
-        container.appendChild(red);
+function escapeHtml(text) {
+    return String(text)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+}
+
+function oznaciTekst(text, query) {
+    var safeText = escapeHtml(text);
+    var safeQuery = query.trim();
+
+    if (safeQuery === '') {
+        return safeText;
     }
-    
+
+    var escapedQuery = safeQuery.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    var regex = new RegExp('(' + escapedQuery + ')', 'gi');
+
+    return safeText.replace(regex, '<mark>$1</mark>');
+}
+
+function filtrirajAutore() {
+    var query = searchInput.value.trim().toLowerCase();
+    currentSearchQuery = query;
+
+    if (query === '') {
+        prikaziAutore(authorIds);
+        return;
+    }
+
+    var searchResults = [];
+
+    for (var id in authors) {
+        var autor = authors[id];
+
+        if (
+            (autor['ime'] && autor['ime'].toLowerCase().includes(query)) ||
+            (autor['prezime'] && autor['prezime'].toLowerCase().includes(query)) ||
+            (autor['status'] && autor['status'].toLowerCase().includes(query))
+        ) {
+            searchResults.push(id);
+        }
+    }
+
+    prikaziAutore(searchResults);
+}
+
+function setupSearch() {
+    searchInput = document.getElementById('search-input');
+    searchBtn = document.getElementById('search-btn');
+
+    if (!searchInput || !searchBtn) {
+        return;
+    }
+
+    searchInput.addEventListener('input', filtrirajAutore);
+    searchBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        filtrirajAutore();
+    });
 }
